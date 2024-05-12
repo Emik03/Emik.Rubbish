a9ed6f8cb70bf0e31399d4e4bcc4313bd8e59c33
Emik <emik.lakunishok@gmail.com>
2024-05-12 22:17:48 +0200

    Add additional documentation

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 # Emik.Rubbish
 
 [![NuGet package](https://img.shields.io/nuget/v/Emik.Rubbish.svg?color=50fa7b&logo=NuGet&style=for-the-badge)](https://www.nuget.org/packages/Emik.Rubbish)
-[![License](https://img.shields.io/github/license/Emik03/rubbish.svg?color=6272a4&style=for-the-badge)](https://github.com/Emik03/Emik.Rubbish/blob/main/LICENSE)
+[![License](https://img.shields.io/github/license/Emik03/Emik.Rubbish.svg?color=6272a4&style=for-the-badge)](https://github.com/Emik03/Emik.Rubbish/blob/main/LICENSE)
 
-Description.
+Cross-platform library to send files to the recycle bin. Supports all major desktop platforms. (Windows, Mac, and Linux)
 
 This project has a dependency to [Emik.Morsels](https://github.com/Emik03/Emik.Morsels), if you are building this project, refer to its [README](https://github.com/Emik03/Emik.Morsels/blob/main/README.md) first.
 
 ---
 
+- [API](#api)
 - [Contribute](#contribute)
 - [License](#license)
 
 ---
+
+## API
+
+- [`Emik.Rubbish.Move(string)`](https://github.com/Emik03/Emik.Rubbish/blob/master/Documentation/Rubbish.Move(string).md)
+  - Moves the file or directory to the recycling bin.
+
+- [`Emik.Rubbish.MoveAsync(string, CancellationToken)`](https://github.com/Emik03/Emik.Rubbish/blob/master/Documentation/Rubbish.MoveAsync(string%2CCancellationToken).md)
+  - Moves the file or directory to the recycling bin asynchronously.
 
 ## Contribute
 
--- Source/Rubbish.cs
+++ Source/Rubbish.cs
@@ -1,46 +1,76 @@
 // SPDX-License-Identifier: MPL-2.0
 namespace Emik;
 
-/// <summary>Allows for trashing files.</summary>
+/// <summary>
+/// Contains the methods <see cref="Move"/> and <see cref="MoveAsync"/> for sending files to the recycling bin.
+/// </summary>
 public static class Rubbish
 {
 #if !NET5_0_OR_GREATER
     static readonly OSPlatform s_bsd = OSPlatform.Create("FREEBSD");
 #endif
 
-    /// <summary>Trashes the file.</summary>
-    /// <param name="path">The path to trash.</param>
+    /// <summary>Moves the file or directory to the recycling bin.</summary>
+    /// <remarks><para>Unlike other IO operations, this method does not ever throw.</para></remarks>
+    /// <example><para>
+    /// The following example moves the file or directory <c>test.txt</c>
+    /// (which is relative to the current working directory) to the recycling bin.
+    /// </para><code language="csharp">
+    /// using System;
+    /// using Emik;
+    /// &#0;
+    /// if (Rubbish.Move("test.txt"))
+    ///     Console.WriteLine("Sent text.txt to the recycle bin.");
+    /// else
+    ///     Console.WriteLine("Failed to move test.txt.");
+    /// </code></example>
+    /// <param name="path">The path to move to the recycling bin. This can be relative or absolute.</param>
     /// <returns>
     /// The value <see langword="true"/> if the operation was successful, otherwise; <see langword="false"/>.
     /// </returns>
-    public static bool Move(string path) =>
+    public static bool Move([NotNullWhen(true)] string? path) =>
+        path is not null &&
 #if NET5_0_OR_GREATER
-        OperatingSystem.IsWindows() ? WindowsRubbish.Move(path) :
+        (OperatingSystem.IsWindows() ? WindowsRubbish.Move(path) :
         OperatingSystem.IsMacOS() ? OsxRubbish.Move(path) :
-        (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD()) && FreedesktopRubbish.Move(path);
+        (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD()) && FreedesktopRubbish.Move(path));
 #else
-        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? WindowsRubbish.Move(path) :
+        (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? WindowsRubbish.Move(path) :
         RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? OsxRubbish.Move(path) :
         (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(s_bsd)) &&
-        FreedesktopRubbish.Move(path);
+        FreedesktopRubbish.Move(path));
 #endif
 
-    /// <summary>Trashes the file asynchronously.</summary>
-    /// <param name="path">The path to trash.</param>
+    /// <summary>Moves the file or directory to the recycling bin asynchronously.</summary>
+    /// <remarks><para>Unlike other IO operations, this method does not ever throw.</para></remarks>
+    /// <example><para>
+    /// The following example moves the file or directory <c>test.txt</c>
+    /// (which is relative to the current working directory) to the recycling bin.
+    /// </para><code language="csharp">
+    /// using System;
+    /// using Emik;
+    /// &#0;
+    /// if (await Rubbish.MoveAsync("test.txt"))
+    ///     Console.WriteLine("Sent text.txt to the recycle bin.");
+    /// else
+    ///     Console.WriteLine("Failed to move test.txt.");
+    /// </code></example>
+    /// <param name="path">The path to move to the recycling bin. This can be relative or absolute.</param>
     /// <param name="token">The token to cancel the operation.</param>
     /// <returns>
-    /// The <see cref="Task{TResult}"/> that trashes the file, returning the value <see langword="true"/>
+    /// The <see cref="Task{TResult}"/> responsible for the operation, returning the value <see langword="true"/>
     /// if the operation was successful, otherwise; <see langword="false"/>.
     /// </returns>
-    public static async Task<bool> MoveAsync(string path, CancellationToken token = default) =>
+    public static async Task<bool> MoveAsync([NotNullWhen(true)] string? path, CancellationToken token = default) =>
+        path is not null &&
 #if NET5_0_OR_GREATER
-        OperatingSystem.IsWindows() ? WindowsRubbish.Move(path) :
+        (OperatingSystem.IsWindows() ? WindowsRubbish.Move(path) :
         OperatingSystem.IsMacOS() ? await OsxRubbish.MoveAsync(path, token) :
-        (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD()) && await FreedesktopRubbish.MoveAsync(path, token);
+        (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD()) && await FreedesktopRubbish.MoveAsync(path, token));
 #else
-        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? WindowsRubbish.Move(path) :
+        (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? WindowsRubbish.Move(path) :
         RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? OsxRubbish.Move(path) :
         (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(s_bsd)) &&
-        await FreedesktopRubbish.MoveAsync(path, token);
+        await FreedesktopRubbish.MoveAsync(path, token));
 #endif
 }
